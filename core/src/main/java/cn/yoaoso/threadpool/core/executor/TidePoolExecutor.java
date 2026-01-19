5295b4ce9f30117b92d1a24cba6b239f30370155
Yifan Qin <2267198159@qq.com>
2026-01-20 03:39:38 +0800

    feat: 静态代理实现线程池拒绝任务的统计与实时告警功能

--- core/src/main/java/cn/yoaoso/threadpool/core/executor/TidePoolExecutor.java
+++ core/src/main/java/cn/yoaoso/threadpool/core/executor/TidePoolExecutor.java
@@ -46,12 +46,33 @@ public class TidePoolExecutor extends ThreadPoolExecutor {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
 
         //TODO 通过动态代理设置拒绝策略执行次数
+        setRejectedExecutionHandler(handler);
 
         //设置动态线程池扩展属性：线程池ID标识
         this.threadPoolId = threadPoolId;
 
         //设置等待终结时间，单位毫秒
         this.awaitTerminationMillis = awaitTerminationMillis;
+    }
+
+
+    @Override
+    public void setRejectedExecutionHandler(RejectedExecutionHandler handler) {
+        RejectedExecutionHandler handlerWrapper = new RejectedExecutionHandler() {
+            @Override
+            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+                rejectCount.incrementAndGet();
+                handler.rejectedExecution(r, executor);
+
+            }
+
+            @Override
+            public String toString() {
+                return handler.getClass().getSimpleName();
+            }
+        };
+
+        super.setRejectedExecutionHandler(handlerWrapper);
     }
 
 
